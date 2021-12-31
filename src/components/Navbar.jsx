0dfcde7da80fdd256b8e56d64583744b7c518fd3
Jay <jay.lee.2280@gmail.com>
2021-12-31 11:05:26 +0900

    Use styled components to navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,53 +3,50 @@ import logo from '.././src_assets/mountain-logo.png';
 import { Link } from 'react-router-dom';
 import { Menu, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 const Navbar = () => {
   const [current, setCurrent] = useState('main');
 
   const handleClick = e => {
-    console.log('click ', e);
     setCurrent(e.key);
   };
   return (
-    <Menu
-      onClick={handleClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      style={{
-        padding: '10px',
-        fontSize: '18px',
-        fontWeight: 'bold',
-        marginBottom: '30px',
-        backgroundColor: '#B5D2B9',
-      }}
-    >
-      <Menu.Item key="main">
-        <Link to="/">
-          <img src={logo} alt="logo" style={{ width: '50px' }} />
-        </Link>
-      </Menu.Item>
+    <Nav>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="main">
+          <Link to="/">
+            <img src={logo} alt="logo" style={{ width: '50px' }} />
+          </Link>
+        </Menu.Item>
 
-      <Menu.Item key="list">
-        <Link to="/list">산 검색</Link>
-      </Menu.Item>
+        <Menu.Item key="list">
+          <Link to="/list">산 검색</Link>
+        </Menu.Item>
 
-      <Menu.Item key="community">
-        <Link to="/community">커뮤니티</Link>
-      </Menu.Item>
+        <Menu.Item key="community">
+          <Link to="/community">커뮤니티</Link>
+        </Menu.Item>
 
-      <Menu.Item key="login">
-        <Link to="/login">
-          <Avatar
-            style={{
-              backgroundColor: '#305136',
-              cursor: 'pointer',
-            }}
-            icon={<UserOutlined />}
-          />
-        </Link>
-      </Menu.Item>
-    </Menu>
+        <Menu.Item key="login">
+          <Link to="/login">
+            <Avatar
+              style={{
+                backgroundColor: 'var(--color-dark-green)',
+                cursor: 'pointer',
+              }}
+              icon={<UserOutlined />}
+            />
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Nav>
   );
 };
 
