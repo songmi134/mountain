0eff76eca86e22965d297cbe6cf4b57f319d588d
Jay <jay.lee.2280@gmail.com>
2022-01-03 23:30:37 +0900

    Add writing form to Community page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Landing from './pages/Landing/Landing';
 import Search from './pages/Search/Search';
 import Community from './pages/Community/Community';
+import WritingForm from './pages/Community/WritingForm';
 import './App.less';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" exact component={Landing} />
       <Route path="/search" exact component={Search} />
       <Route path="/community" exact component={Community} />
+      <Route path="/form" exact component={WritingForm} />
     </BrowserRouter>
   );
 };
