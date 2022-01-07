2b180360b5a23ba9196fe5d7b2108d608474f903
Jay <jay.lee.2280@gmail.com>
2022-01-07 23:09:01 +0900

    Update App.js : Routing

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Search from './pages/Search/Search';
 import Community from './pages/Community/Community';
 import WritingForm from './pages/Community/WritingForm';
 import CommunityDetail from './pages/Community/Detail';
+import My from './pages/My/My';
 import './App.less';
 
 const App = () => {
   return (
-    <BrowserRouter basename="mountain">
+    <BrowserRouter basename="pages">
       <Navbar />
       <Route path="/" exact component={Landing} />
       <Route path="/search" exact component={Search} />
       <Route path="/community" exact component={Community} />
       <Route path="/form" exact component={WritingForm} />
       <Route path="/community/detail" component={CommunityDetail} />
+      <Route path="/my" component={My} />
     </BrowserRouter>
   );
 };
