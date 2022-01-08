e9df1ec1dd2ae14e7729a1a043eb0502d3f2b051
skdksldk1485 <skdksldk1485@naver.com>
2022-01-08 17:05:49 +0900

    Test login api

--- src/components/login/AuthProvider.js
+++ src/components/login/AuthProvider.js
@@ -13,20 +13,29 @@ const AuthProvider = ({ children }) => {
       if(firebaseUser) {
         const token = await firebaseUser.getIdToken();
         defaultHeaders.Authorization = `Bearer ${token}`;
-        const res = await fetch("/users/me", {
+        console.log(token);
+        //https://cors-anywhere.herokuapp.com/
+        const res = await fetch("https://moutain.herokuapp.com/users/me", {
+          mode: 'no-cors',
           method: "GET",
           headers: defaultHeaders,
         });
+        console.log(res);
+        console.log('1 : '+res.status);
         if(res.status === 200) {
+          console.log('2 : ');
           const user = await res.json();
           setUser(user);
         } else if (res.status === 401) {
+          console.log('3 : ');
           const data = await res.json();
           if(data.code === "USER_NOT_FOUND") {
+            console.log('4 : ');
             setRegisterFormOpen(true);
           } 
         } 
       } else {
+        console.log('5 : ');
         delete defaultHeaders.Authorizations;
         setUser(null);
       } 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../components/login/AuthProvider';
 import { signInGoogle, signOut } from '../../auth/firebaseAuth';
 import '../../index.css';
+import firebase from "firebase";
+import 'firebase/auth';
 
+//var firebaseui = require('firebaseui');
 
 const Login = () => {
   const { user }  = useContext(UserContext);
+/*
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '/login',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+*/
   return (
-    
+      <>
       <div>
         {user ? ( <div className='main'> <p>{user.nickname} <br/> {user.email} </p> </div> ) : 
-          ( <div className='main'> <p>Sign in With Google</p> </div> )}
+          ( <div className='main'> <p>로그인을 해주세요</p> </div> )}
         {user ? (  <button className='signin' onClick={signOut}>Sign Out</button> ) : 
           ( 
           <button className='signin' onClick={signInGoogle}>Sign in With Google</button> )}
       </div>
-      
+      <div className='common__form__join'>
+        계정이 없으십니까?{' '}
+        <Link to={'/register'}>
+          회원가입
+        </Link>
+      </div>
+    </>
   );
 }
 
