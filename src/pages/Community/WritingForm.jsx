5b91d170b847c032b41e538a433f61087b75cb5c
Jay <jay.lee.2280@gmail.com>
2022-01-18 17:02:35 +0900

    Update writing form

--- src/pages/Community/WritingForm.jsx
+++ src/pages/Community/WritingForm.jsx
@@ -3,13 +3,34 @@ import { Form, Input, Row, Radio } from 'antd';
 import { FormContainer, FormInput, FormButton } from './Community.style';
 import axios from 'axios';
 import moment from 'moment';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 const WritingForm = () => {
   const [categories, setCategories] = useState(undefined);
   const [category, setCategory] = useState(undefined);
   const history = useHistory();
-  const state = history.location.state;
+
+  const params = useParams();
+  const postId = params.id;
+  const [post, setPost] = useState(undefined);
+
+  // TODO : 1/18 8:00PM
+  // post를 비동기로 가져오게 되면서 생긴 문제 - 글 수정 시 카테고리 정보를 늦게 가져오게 됨
+  // post가 있을 때와 없을 때의 차이를 어떻게 반영할 것인가?
+  useEffect(() => {
+    let completed = false;
+    const getPost = async () => {
+      const response = await axios.get(`/communities/${postId}`);
+      if (!completed) {
+        setPost(response.data);
+        console.log(response.data);
+      }
+    };
+    getPost();
+    return () => {
+      completed = true;
+    };
+  }, []);
 
   useEffect(() => {
     let completed = false;
@@ -28,7 +49,8 @@ const WritingForm = () => {
   const handleCategoryChange = v => setCategory(v.target.value);
 
   const onFinish = async values => {
-    const createdAt = moment().format('YYYY.MM.DD HH:mm:ss');
+    // TODO: 로그인 붙인 이후, createAt 제거한 후 잘 되는지 확인
+    const createdAt = moment().format('YYYY.MM.DD HH:mm:ss'); //백엔드 DB 저장시 - 백엔드에서 할지 or 프론트에서 한다면 어떤 format으로 할지
     const newPost = {
       ...values,
       createdAt,
@@ -37,7 +59,7 @@ const WritingForm = () => {
       writer: { name: '닉네임' },
     };
     try {
-      await axios.post('http://localhost:4000/community', newPost);
+      await axios.post('/communities', newPost);
       history.push('/community'); // 현재 생성된 포스트의 id를 알아내서 해당 detail 페이지로 이동시키기
     } catch (err) {
       console.log(err);
@@ -45,12 +67,14 @@ const WritingForm = () => {
   };
 
   const handleEdit = async values => {
-    const id = state.id;
     const updatedAt = moment().format('YYYY.MM.DD HH:mm:ss');
     const updatedPost = { ...values, updatedAt, category };
     try {
-      await axios.patch(`http://localhost:4000/community/${id}`, updatedPost);
-      history.push(`/community/detail/${id}`);
+      await axios.patch(
+        `http://localhost:4000/community/${postId}`,
+        updatedPost
+      );
+      history.push(`/community/detail/${postId}`);
     } catch (err) {
       console.log(err);
     }
@@ -59,14 +83,14 @@ const WritingForm = () => {
   return (
     <Row>
       <FormContainer
-        onFinish={state ? handleEdit : onFinish}
+        onFinish={post ? handleEdit : onFinish}
         fields={[
-          { name: ['title'], value: state?.title },
-          { name: ['content'], value: state?.content },
+          { name: ['title'], value: post?.title },
+          { name: ['content'], value: post?.content },
         ]}
       >
         <Radio.Group
-          defaultValue={state ? state.category : '산 후기'}
+          defaultValue={post ? post.category : '산 후기'}
           buttonStyle="solid"
           onChange={handleCategoryChange}
         >
