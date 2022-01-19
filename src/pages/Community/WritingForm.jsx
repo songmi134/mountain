363f88f50a5440ffaa3fd236e0e4b846c9b40fde
Jay <jay.lee.2280@gmail.com>
2022-01-19 23:25:38 +0900

    Update category logic : use Form.item

--- src/pages/Community/WritingForm.jsx
+++ src/pages/Community/WritingForm.jsx
@@ -6,24 +6,18 @@ import moment from 'moment';
 import { useHistory, useParams } from 'react-router-dom';
 
 const WritingForm = () => {
-  const [categories, setCategories] = useState(undefined);
-  const [category, setCategory] = useState(undefined);
   const history = useHistory();
-
   const params = useParams();
   const postId = params.id;
   const [post, setPost] = useState(undefined);
+  const [categories, setCategories] = useState(undefined);
 
-  // TODO : 1/18 8:00PM
-  // post를 비동기로 가져오게 되면서 생긴 문제 - 글 수정 시 카테고리 정보를 늦게 가져오게 됨
-  // post가 있을 때와 없을 때의 차이를 어떻게 반영할 것인가?
   useEffect(() => {
     let completed = false;
     const getPost = async () => {
       const response = await axios.get(`/communities/${postId}`);
       if (!completed) {
         setPost(response.data);
-        console.log(response.data);
       }
     };
     getPost();
@@ -46,18 +40,17 @@ const WritingForm = () => {
     };
   }, []);
 
-  const handleCategoryChange = v => setCategory(v.target.value);
-
   const onFinish = async values => {
     // TODO: 로그인 붙인 이후, createAt 제거한 후 잘 되는지 확인
     const createdAt = moment().format('YYYY.MM.DD HH:mm:ss'); //백엔드 DB 저장시 - 백엔드에서 할지 or 프론트에서 한다면 어떤 format으로 할지
+    // TODO : 형식 맞추기
     const newPost = {
       ...values,
       createdAt,
-      category,
       viewCount: 0,
       writer: { name: '닉네임' },
     };
+    console.log(newPost);
     try {
       await axios.post('/communities', newPost);
       history.push('/community'); // 현재 생성된 포스트의 id를 알아내서 해당 detail 페이지로 이동시키기
@@ -68,18 +61,18 @@ const WritingForm = () => {
 
   const handleEdit = async values => {
     const updatedAt = moment().format('YYYY.MM.DD HH:mm:ss');
-    const updatedPost = { ...values, updatedAt, category };
+    const updatedPost = { ...values, updatedAt };
     try {
       await axios.patch(
         `http://localhost:4000/community/${postId}`,
         updatedPost
       );
-      history.push(`/community/detail/${postId}`);
+      history.push(`/community/${postId}`);
     } catch (err) {
       console.log(err);
     }
   };
-
+  console.log('POST', post);
   return (
     <Row>
       <FormContainer
@@ -87,23 +80,25 @@ const WritingForm = () => {
         fields={[
           { name: ['title'], value: post?.title },
           { name: ['content'], value: post?.content },
+          { name: ['category'], value: post?.category },
         ]}
       >
-        <Radio.Group
-          defaultValue={post ? post.category : '산 후기'}
-          buttonStyle="solid"
-          onChange={handleCategoryChange}
+        <Form.Item
+          name="category"
+          rules={[{ required: true, message: '카테고리를 선택하세요!' }]}
         >
-          {categories ? (
-            categories.content.map(v => (
-              <Radio.Button key={v.cateId} value={v.cateName}>
-                {v.cateName}
-              </Radio.Button>
-            ))
-          ) : (
-            <Row>Loading...</Row>
-          )}
-        </Radio.Group>
+          <Radio.Group buttonStyle="solid">
+            {categories ? (
+              categories.content.map(v => (
+                <Radio.Button key={v.cateId} value={v.cateName}>
+                  {v.cateName}
+                </Radio.Button>
+              ))
+            ) : (
+              <Row>Loading...</Row>
+            )}
+          </Radio.Group>
+        </Form.Item>
         <Form.Item
           name="title"
           rules={[{ required: true, message: '제목을 입력하세요!' }]}
