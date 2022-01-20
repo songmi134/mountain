8ef5905357b6554e981e98ff7e8f4c19e8f60564
skdksldk1485 <skdksldk1485@naver.com>
2022-01-20 23:02:37 +0900

    Fix bugs(Could not find FirebaseUI widget error)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,46 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../index.css';
-//import firebase from "firebase";
 import 'firebase/auth';
 import { Container, Title } from './Login.style';
-
 import {ui, uiConfig} from '../../auth/firebaseAuth';
 
-//var firebaseui = require('firebaseui');
-
 const Login = () => {
-/*
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '/login',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-  };
+  useEffect(() => {
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }, []);
 
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-*/
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
   return (
       <>
         <Container>
